cb36734bb6d53e6251e86a5ffab55e42e35cd6fb
vidushisinghh11 <vidushis011@gmail.com>
2026-02-15 21:54:07 +0530

    Fix TypeScript types in API routes

--- pages/api/poll/[id].ts
+++ pages/api/poll/[id].ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { supabase } from '../../../lib/supabase'
-import type { NextApiRequest, NextApiResponse } from 'next'
+
 
 
 export default async function handler(
